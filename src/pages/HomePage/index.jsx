eb692da1431c2ae7e7723640b209d17969dbf581
AdrianAmerico <1fnadrian1@gmail.com>
2022-04-01 15:05:01 -0300

    widgets

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { SideBar } from "../../components/SideBar"
 import { Navbar } from "../../components/NavBar"
+import { Widget } from "../../components/Widget"
 import "./home.scss"
 
 const HomePage = () => {
@@ -9,6 +10,12 @@ const HomePage = () => {
             <SideBar />
             <div className="homeContainer">
                 <Navbar />
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
                 Home container
             </div>
         </div>
